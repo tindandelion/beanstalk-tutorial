e3902e0391d2025b912f20e0500632f0f7fd7f6d
Sergey Moshnikov <s.moshnikov@gmail.com>
2021-10-09 16:23:17 +0000

    Starting the application

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Upload playdata files to S3
+        </v-card-title>
+        <v-card-text>
+          <v-file-input
+            v-model="fileToUpload"
+            :label="'File to upload'"
+          ></v-file-input>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" @click="upload"> Upload </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fileToUpload: null,
+    }
+  },
+  methods: {
+    upload() {
+      this.$axios.$post('/upload').catch((e) => {
+        console.log('Error: ' + e)
+      })
+    },
+  },
+}
+</script>
