d9fd710ddf07f53be5f44d47edf81ea5127a99b5
Sergey Moshnikov <s.moshnikov@gmail.com>
2021-10-17 06:05:07 +0000

    Updated the label

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Upload playdata files to S3
+          Upload files to S3
         </v-card-title>
         <v-card-text>
           <v-file-input
